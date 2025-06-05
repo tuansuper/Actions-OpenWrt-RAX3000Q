23cd203e6ddc9a6da2c530eba784fe17be64a9b0
tuansuper <144403817+tuansuper@users.noreply.github.com>
2025-06-05 15:47:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,11 +9,13 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-# bg1 directory
-cd $GITHUB_WORKSPACE/openwrt/feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img
-# remove  bg1.jpg file
-rm bg1.jpg
-# copy background file
-cd $GITHUB_WORKSPACE/openwrt/feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background
-cp $GITHUB_WORKSPACE/resources/background/* .
-[ -e README.md ] && rm README.md
+if [ $CUSTOM_ARGON_BG == "true" ]; then
+  # bg1 directory
+  cd $GITHUB_WORKSPACE/openwrt/feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img
+  # remove  bg1.jpg file
+  rm bg1.jpg
+  # copy background file
+  cd $GITHUB_WORKSPACE/openwrt/feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/background
+  cp $GITHUB_WORKSPACE/resources/background/* .
+  [ -e README.md ] && rm README.md
+fi
